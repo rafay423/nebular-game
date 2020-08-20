694b828e2100246e49ca4a61332f199afff200fd
rafay423 <66517722+rafay423@users.noreply.github.com>
2020-08-20 09:56:39 +0500

    Initial commit

---
+++ Assets/bullets1peed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullets1peed : MonoBehaviour {
+	public Rigidbody bullets;
+		public Transform gun;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			Rigidbody project;
+			project = Instantiate(bullets, gun.position, gun.rotation) as Rigidbody;
+			project.AddForce(gun.forward * 350f);
+			
+		}
+	}
+}
+
---
+++ Assets/bulletspeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletspeed : MonoBehaviour {
+	public Rigidbody bullets;
+		public Transform gun;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			Rigidbody project;
+			project = Instantiate(bullets, gun.position, gun.rotation) as Rigidbody;
+			project.AddForce(gun.forward * 350f);
+		}
+	}
+}
